b885704a4bae41518b2946a3dc54266db6e7e974
jayasekhar <noreply@dummy.noreply.com>
2020-03-22 18:19:23 +0530

    Override properties and print different kinds of properties

--- properties/PropertySourceEx1/src/main/java/com/explore/jk/spring/Application.java
+++ properties/PropertySourceEx1/src/main/java/com/explore/jk/spring/Application.java
@@ -6,7 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.AbstractEnvironment;
+import org.springframework.core.env.EnumerablePropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.support.ResourcePropertySource;
+
+import java.util.Iterator;
 
 @ComponentScan(basePackages = "com.explore.jk.spring")
 public class Application {
@@ -41,5 +48,37 @@ public class Application {
         logger.info("Using @Value injection");
         logger.info("Application name: {}", appName);
         logger.info("Application version: {}", appVersion);
+        printResourceProperties(env);
+
+        printNonResourceProperties(env);
     }
+
+    private void printResourceProperties(Environment env) {
+        MutablePropertySources propertySources = ((AbstractEnvironment) env).getPropertySources();
+        Iterator<PropertySource<?>> sources = (Iterator<PropertySource<?>>) propertySources.iterator();
+        while (sources.hasNext()) {
+            PropertySource<?> source = sources.next();
+            if(source instanceof ResourcePropertySource) {
+                logger.info("********************** {} ******************************", source.getName());
+                for(String property : ((EnumerablePropertySource<?>) source).getPropertyNames()) {
+                    logger.info("{} : {}", property, source.getProperty(property));
+                }
+            }
+        }
+    }
+
+    private void printNonResourceProperties(Environment env) {
+        MutablePropertySources propertySources = ((AbstractEnvironment) env).getPropertySources();
+        Iterator<PropertySource<?>> sources = (Iterator<PropertySource<?>>) propertySources.iterator();
+        while (sources.hasNext()) {
+            PropertySource<?> source = sources.next();
+            if(!(source instanceof ResourcePropertySource)) {
+                logger.info("********************** {} ******************************", source.getName());
+                for(String property : ((EnumerablePropertySource<?>) source).getPropertyNames()) {
+                    logger.info("{} : {}", property, source.getProperty(property));
+                }
+            }
+        }
+    }
+
 }
--- properties/PropertySourceEx1/src/main/java/com/explore/jk/spring/config/AppConfig.java
+++ properties/PropertySourceEx1/src/main/java/com/explore/jk/spring/config/AppConfig.java
@@ -3,8 +3,9 @@ package com.explore.jk.spring.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-@Configuration
+@Configuration//,override.properties
 @PropertySource(value = "application.properties", ignoreResourceNotFound = true)
+@PropertySource(value = "override.properties", ignoreResourceNotFound = true)
 public class AppConfig {
 
 }
