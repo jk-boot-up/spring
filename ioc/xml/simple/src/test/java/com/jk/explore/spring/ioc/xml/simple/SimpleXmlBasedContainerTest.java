3d3898ff9b2701c37f5b63599d452eeb316c2ab9
jayasekhar <do_not_send_mail@dummy_dnd.com>
2023-04-09 16:28:14 +0530

    Add variants for bean creation from constructor arguments

--- ioc/xml/simple/src/test/java/com/jk/explore/spring/ioc/xml/simple/SimpleXmlBasedContainerTest.java
+++ ioc/xml/simple/src/test/java/com/jk/explore/spring/ioc/xml/simple/SimpleXmlBasedContainerTest.java
@@ -3,6 +3,12 @@ package com.jk.explore.spring.ioc.xml.simple;
 import com.jk.explore.spring.ioc.xml.simple.domain.Domain;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -11,7 +17,7 @@ class SimpleXmlBasedContainerTest {
 
     @Test
     void context() {
-        SimpleXmlBasedContainer container = new SimpleXmlBasedContainer("/simpleContext.xml");
+        SimpleXmlBasedContainer container = new SimpleXmlBasedContainer("/xml/context/simpleContext.xml");
         assertNotNull(container);
         ClassPathXmlApplicationContext context = container.getContext();
         assertNotNull(context);
@@ -21,6 +27,24 @@ class SimpleXmlBasedContainerTest {
         assertNotNull(domain);
         assertEquals(myDomain, domain);
         org.assertj.core.api.Assertions.assertThat(context.containsBean("myDomain")).isTrue();
+    }
+
+    @Test
+    void resourceCreation() throws IOException {
+        URL url = new URL("https", "google.com", "");
+        Resource urlResource = new UrlResource(url);
+        byte[] bytes = urlResource.getContentAsByteArray();
+        String content = new String(bytes, StandardCharsets.UTF_8);
+        assertNotNull(content);
+    }
+
+    @Test
+    void resourceCreationFromXmlConfig() {
+        SimpleXmlBasedContainer container = new SimpleXmlBasedContainer("xml/context/url-resource-creation.xml");
+        URL url = container.getContext().getBean("googleHomePageUrl", URL.class);
+        assertNotNull(url);
+        UrlResource urlResource = container.getContext().getBean("googleUrlResource", UrlResource.class);
+        assertNotNull(urlResource);
     }
 
 }
